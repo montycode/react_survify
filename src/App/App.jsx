82a0650cd2c845c61065c837b78409757212e119
Omar Montoya <alfredo.montoya@cesunbc.edu.mx>
2020-12-30 10:49:52 -0800

    Create App

---
+++ src/App/App.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Router, Route } from "react-router-dom";
+
+import { history } from "@/_helpers";
+import { authenticationService } from "@/_services";
+import { PrivateRoute, Header, Footer } from "@/_components";
+import { HomePage, LoginPage } from "@/Pages";
+
+import "tailwindcss/tailwind.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  componentDidMount() {
+    authenticationService.currentUser.subscribe((x) =>
+      this.setState({ currentUser: x })
+    );
+  }
+
+  render() {
+    const { currentUser } = this.state;
+    return (
+      <Router history={history}>
+        <div className="flex flex-col min-h-screen">
+          {currentUser && <Header />}
+          <PrivateRoute exact path="/" component={HomePage} />
+          <Route path="/login" component={LoginPage} />
+          {currentUser && <Footer />}
+        </div>
+      </Router>
+    );
+  }
+}
+
+export { App };
